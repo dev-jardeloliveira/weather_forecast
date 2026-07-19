77bd793529c4acc25f55bda686ff95a6b5853260
Jardel Oliveira <dev.jardeloliveira@gmail.com>
2026-07-19 12:31:41 -0300

    test integration

--- test/api_integrate_test.dart
+++ test/api_integrate_test.dart
@@ -20,5 +20,31 @@ void main() {
       expect(response.statusCode, equals(200));
       expect(response.data, isNotNull);
     });
+
+    test('Deve lançar exception', () async {
+      try {
+        final invalidDio = await _createInvalidKey();
+
+        final response = await invalidDio.get(
+          AppEndpoint.current,
+          queryParameters: {'q': 'Ponte Nova'},
+        );
+        print(response);
+        fail('Deveria falhar');
+      } on DioException catch (e) {
+        expect(e, isA<DioException>());
+        expect(e.response?.statusCode, 401);
+      }
+    });
   });
+}
+
+Future<Dio> _createInvalidKey() async {
+  final key = dotenv.env['WEATHER_API_KEY'];
+  dotenv.env['WEATHER_API_KEY'] = 'invalid_key';
+  final dio = DioClient.getInstance();
+  if (key != null) {
+    dotenv.env['WEATHER_API_KEY'] = key;
+  }
+  return dio;
 }
